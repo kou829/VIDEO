abb26f3b6a4e8cc750e08411a5f1f19a3461fdb1
kou829 <73116796+kou829@users.noreply.github.com>
2021-01-19 12:02:40 +0900

    コントローラーの変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(video_id: params[:video_id])
     if @like.save
-      flash[:success] = "いいね登録しました"
       redirect_to "/videos/#{params[:video_id]}"
     end
   end
@@ -10,7 +9,6 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(user_id: current_user.id, video_id: params[:video_id])
     if @like.destroy
-      flash[:success] = "いいね解除しました"
       redirect_to "/videos/#{params[:video_id]}"
     end
   end
